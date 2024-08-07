50f532cebbfc96c961908760ddba304888517882
𖤍ᴛʜᴇ / ᴄᴀᴘᴛᴀɪɴ𖤍 <167273277+itzAsuraa@users.noreply.github.com>
2024-08-07 17:19:04 +0530

    Update connection_sql.py

--- MahakRobot/modules/sql/connection_sql.py
+++ MahakRobot/modules/sql/connection_sql.py
@@ -4,7 +4,7 @@ from typing import Union
 
 from sqlalchemy import BigInteger, Boolean, Column, String, UnicodeText
 
-from MahakRobot.modules.sql import BASE, SESSION
+from MahakRobot.sql import BASE, SESSION
 
 
 class ChatAccessConnectionSettings(BASE):
