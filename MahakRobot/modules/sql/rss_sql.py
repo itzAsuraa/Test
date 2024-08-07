5d4502129c25b14603169dc9f22ebebf696876b6
𖤍ᴛʜᴇ / ᴄᴀᴘᴛᴀɪɴ𖤍 <167273277+itzAsuraa@users.noreply.github.com>
2024-08-07 17:44:28 +0530

    Update rss_sql.py

--- MahakRobot/modules/sql/rss_sql.py
+++ MahakRobot/modules/sql/rss_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import BigInteger, Column, UnicodeText
 
-from MahakRobot.modules.sql import BASE, SESSION
+from MahakRobot.sql import BASE, SESSION
 
 
 class RSS(BASE):
